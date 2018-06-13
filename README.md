38a9cb446e270e27c20b7023977b80d0acd04a6b
Dmitriy Karmalita <kard@ciklum.com>
2018-06-13 18:52:33 +0300

    [Reviewed]

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-Simple Lambda example
+# Simple Lambda example
+
+A simple Lambda service that allows sending a post request to an API and returns it's response.
+
+A simple Lambda service that allows sending a post request to an API and returns it's response. It uses [jsonplaceholder](https://jsonplaceholder.typicode.com/) as the API server for this example.
+
+## How to run
 
 In the terminal, run these commands:
 ```sh
+git clone https://github.com/dkarmalita/lambda-get-example.git
+cd lambda-get-example
 npm i
 npm start
 ```
 
-In the second terminal window, execute this:
+In the secondary terminal window, execute this:
 ```sh
 open http://localhost:3000/hello/get
 ```
--- my-service/handler.js
+++ my-service/handler.js
@@ -1,17 +1,84 @@
 'use strict';
-require('../lib/getApi.js')
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+
+  var postData = querystring.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+  });
+
+  post(postData).then(
+    res => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Go Serverless v1.0! Your function executed successfully!',
+          statusCode: res.statusCode,
+          headers: res.headers,
+          data: res.data.toString('utf8'),
+          // input: event,
+        }),
+      };
+      callback(null, response)
+    },
+  ).catch( err => {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err
+      }),
+    };
+    callback(null, response)
+  } )
+
+};
+
+// ------ lib ------- //
+
+const querystring = require('querystring');
+const https = require('https');
+
+/**
+ * Sends a post request to api
+ * @param  {Object} data - data object to post
+ * @return {Promise} - responce object on resolve / error on reject
+ */
+function post(data){
+
+  /**
+   * Use the options to configure your request
+   * @type {Object}
+   */
+  var options = {
+    hostname: 'jsonplaceholder.typicode.com',
+    port: 443,
+    path: '/posts',
+    method: 'POST',
+    headers: {
+     'Content-Type': 'application/x-www-form-urlencoded',
+     'Content-Length': data.length
+    }
   };
 
-  callback(null, response);
+  return new Promise( (resolve, reject) => {
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+    var req = https.request(options, (res) => {
+      res.on('data', (d) => {
+        resolve({
+          statusCode: res.statusCode,
+          headers: res.headers,
+          data: d,
+        })
+      });
+    });
+
+    req.on('error', (e) => {
+      reject(e);
+    });
+
+    req.write(data);
+    req.end();
+
+  } )
+}
